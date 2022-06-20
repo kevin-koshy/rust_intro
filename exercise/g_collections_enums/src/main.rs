21174503717594a0e3d48be84a5429f8e848701b
Kevin Koshy <kevin_koshy@hotmail.com>
2022-06-20 20:42:08 +0530

    General updates

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -1,6 +1,9 @@
 // Silence some warnings that could distract from the exercise
 #![allow(unused_variables, unused_mut, dead_code)]
 
+use std::time::Duration;
+use serialport::{available_ports};
+
 // Someone is shooting arrows at a target.  We need to classify the shots.
 //
 // 1a. Create an enum called `Shot` with variants:
@@ -9,6 +12,11 @@
 // - `Miss`
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
 
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
@@ -18,17 +26,34 @@ impl Shot {
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+
+    match self {
+        Shot::Bullseye => 5,
+        Shot::Hit(x) => {
+            if x < 3.0 {
+                2
+            } else {
+                1
+            }
+        },
+        Shot::Miss => 0
+    }
     }
 }
 
 fn main() {
     // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
+
     let arrow_coords: Vec<Coord> = get_arrow_coords(5);
     let mut shots: Vec<Shot> = Vec::new();
 
     // 2. For each coord in arrow_coords:
     //
     //   A. Call `coord.print_description()`
+     for coord in arrow_coords {
+         coord.print_description();
+     }
+
     //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
     //   `coord.distance_from_center()`
     //      - Less than 1.0 -- `Shot::Bullseye`
@@ -38,7 +63,9 @@ fn main() {
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
-
+    for shot in shots {
+        total += shot.points();
+    }
     println!("Final point total is: {}", total);
 }
 
@@ -75,3 +102,4 @@ fn get_arrow_coords(num: u32) -> Vec<Coord> {
     }
     coords
 }
+
